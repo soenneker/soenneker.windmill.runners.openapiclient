8f8c16d20dda17a695965b3f2a2dcd49b5e0dab7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 11:36:39 -0500

    lifetime issues

--- src/Soenneker.Windmill.Runners.OpenApiClient/Startup.cs
+++ src/Soenneker.Windmill.Runners.OpenApiClient/Startup.cs
@@ -23,12 +23,12 @@ public static class Startup
     public static IServiceCollection SetupIoC(this IServiceCollection services)
     {
         services.AddHostedService<ConsoleHostedService>()
-                .AddScoped<IFileOperationsUtil, FileOperationsUtil>()
-                .AddRunnersManagerAsScoped()
-                .AddFileDownloadUtilAsScoped()
-                .AddYamlUtilAsScoped()
-                .AddOpenApiFixerAsScoped()
-                .AddKiotaUtilAsScoped();
+                .AddSingleton<IFileOperationsUtil, FileOperationsUtil>()
+                .AddRunnersManagerAsSingleton()
+                .AddFileDownloadUtilAsSingleton()
+                .AddYamlUtilAsSingleton()
+                .AddOpenApiFixerAsSingleton()
+                .AddKiotaUtilAsSingleton();
 
         return services;
     }
